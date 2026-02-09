9f4d37c509c4f151fc89aa3dfcf175922f6d2f5a
Itachi917 <87574052+Itachi917@users.noreply.github.com>
2026-02-09 13:37:20 +0400

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCqjKlVjtp7gaEJpQXxVgr1i2xCZUJ7yro",
+  authDomain: "sohar-hall-booking.firebaseapp.com",
+  projectId: "sohar-hall-booking",
+  storageBucket: "sohar-hall-booking.firebasestorage.app",
+  messagingSenderId: "152839243234",
+  appId: "1:152839243234:web:3adddd0567f6631285a5bb"
+};
